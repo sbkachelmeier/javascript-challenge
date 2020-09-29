97f5ee0dacac4fd78004304a338eabe6b5714113
Sarah Kachelmeier <sbkachelmeier@gmail.com>
2020-09-28 21:05:13 -0500

    Got the filter to work

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -5,56 +5,52 @@ var tableData = data;
 var tbody = d3.select("tbody");
 
 // Add the table
-tableData.forEach((UFOsight) => 
-{
+tableData.forEach((UFOsight) => {
     var row = tbody.append("tr");
-    Object.entries(UFOsight).forEach(([key, value]) => 
-    {
-      var cell = row.append("td");
-      cell.text(value);
+    Object.entries(UFOsight).forEach(([key, value]) => {
+        var cell = row.append("td");
+        cell.text(value);
     });
 });
 
 //Make the button
-    var button = d3.select("#filter-btn");
-    var form = d3.select("#form");
+var button = d3.select("#filter-btn");
+var form = d3.select("#form");
 
 
-    // Create event handlers 
-    button.on("click", runEnter);
-    form.on("submit",runEnter);
+// Create event handlers 
+button.on("click", runEnter);
+form.on("submit", runEnter);
 
 //Make the function
-    function runEnter() 
-    {
-// Prevent the page from refreshing
+function runEnter() {
+    // Prevent the page from refreshing
     d3.event.preventDefault();
-    
-// Select the input element and get the raw HTML node
+
+    // Select the input element and get the raw HTML node
     var inputElement = d3.select("#datetime");
 
-// Get the value property of the input element
+    // Get the value property of the input element
     var inputValue = inputElement.property("value");
 
-// Create a custom filtering function
+    // Create a custom filtering function
     function selectDate(sighting) {
-    return sighting.datetime = inputValue;
-}
+        return sighting.datetime === inputValue;
+    }
 
-// filter() uses the custom function as its argument
-var dateFilter = data.filter(selectDate);
+    // filter() uses the custom function as its argument
+    var dateFilter = data.filter(selectDate);
 
-// Test
-console.log(dateFilter);
-
-// Display new table 
-// Add the table
-dateFilter.forEach((sighting) => 
-{
-    var row = tbody.append("tr");
-    Object.entries(sighting).forEach(([key, value]) => 
-    {
-      var cell = row.append("td");
-      cell.text(value);
+    // Test
+    console.log(dateFilter);
+    tbody.html("")
+    // Display new table 
+    // Add the table
+    dateFilter.forEach((sighting) => {
+        var row = tbody.append("tr");
+        Object.entries(sighting).forEach(([key, value]) => {
+            var cell = row.append("td");
+            cell.text(value);
+        });
     });
-});
+}
